3c4d07115ff7cc6016c55dcdd4343b8cfb00ba5a
Mangusthvile <115423180+Mangusthvile@users.noreply.github.com>
2026-01-26 20:34:11 -0600

    changes

--- android/app/src/main/java/com/cmwil/talevox/MainActivity.java
+++ android/app/src/main/java/com/cmwil/talevox/MainActivity.java
@@ -3,23 +3,41 @@ package com.cmwil.talevox;
 import android.content.Intent;
 import android.util.Log;
 import android.os.Bundle;
+import android.os.Build;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginHandle;
 import com.cmwil.talevox.jobrunner.JobRunnerPlugin;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.content.pm.PackageManager;
+
+import android.Manifest;
 import ee.forgr.capacitor.social.login.GoogleProvider;
 import ee.forgr.capacitor.social.login.SocialLoginPlugin;
 import ee.forgr.capacitor.social.login.ModifiedMainActivityForSocialLoginPlugin;
 
 // ModifiedMainActivityForSocialLoginPlugin is VERY VERY important !!!!!!
 public class MainActivity extends BridgeActivity implements ModifiedMainActivityForSocialLoginPlugin {
+  private static final int REQ_POST_NOTIFICATIONS = 1001;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     registerPlugin(JobRunnerPlugin.class);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+      if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS)
+          != PackageManager.PERMISSION_GRANTED) {
+        ActivityCompat.requestPermissions(
+          this,
+          new String[]{Manifest.permission.POST_NOTIFICATIONS},
+          REQ_POST_NOTIFICATIONS
+        );
+      }
+    }
   }
 
   @Override
